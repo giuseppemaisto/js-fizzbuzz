ead2b6ce7dcee518009bb60bf4e4731a1509516e
giuseppemaisto <gmaisto98@gmail.com>
2022-11-29 15:23:44 +0100

    primo commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*
+1-creo ciclo for da 1 a 100;
+2-otilizzo operatore resto 
+SE numero diviso 3 da resto 0 allora stampo Fizz
+SE numero diviso 5 da resto 0 allora stampo Buzz
+SE numero è multiplo sia di 3 che di 5 stampo FizzBuzz
+*/
