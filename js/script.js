9af0bc0d7154fe29fc9f5800d87ce9fd8728ed3f
giuseppemaisto <gmaisto98@gmail.com>
2022-11-29 15:31:38 +0100

    aggiunta stampa in console fizzbuzz e fizz

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@ SE numero diviso 3 da resto 0 allora stampo Fizz
 SE numero diviso 5 da resto 0 allora stampo Buzz
 SE numero è multiplo sia di 3 che di 5 stampo FizzBuzz
 */
+
+
+/*1-creo ciclo for da 1 a 100;*/
+
+for(let i = 1; i <= 100; i++){
+
+    if((i % 3 == 0) && (i % 5 == 0 )){
+        console.log(i + ' FizzBuzz')
+    }
+
+    else if (i % 3 == 0){
+        console.log(i + ' Fizz')
+
+    }
+}
