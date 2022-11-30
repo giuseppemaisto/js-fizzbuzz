7a5db5ff5120cd90a1b2776d00aeafd2c336e1a3
giuseppemaisto <gmaisto98@gmail.com>
2022-11-30 10:11:40 +0100

    terminato con Bonus

--- js/script.js
+++ js/script.js
@@ -7,22 +7,42 @@ SE numero è multiplo sia di 3 che di 5 stampo FizzBuzz
 */
 
 
+const container = document.querySelector('.container')
 /*1-creo ciclo for da 1 a 100;*/
 
 for(let i = 1; i <= 100; i++){
+    let element;
 
+   
+    /*SE numero è multiplo sia di 3 che di 5 stampo FizzBuzz*/
     if((i % 3 == 0) && (i % 5 == 0 )){
-        console.log(i + ' FizzBuzz');
+        let word = 'FizzBuzz'
+        container.innerHTML += "<div class='square square-fizzbuzz'>"+word+"<div>"
+       console.log(i + word)
     }
-
+    /*SE numero diviso 3 da resto 0 allora stampo Fizz*/ 
     else if (i % 3 == 0){
-        console.log(i + ' Fizz');
+        let word = 'Fizz'
+        container.innerHTML += "<div class='square square-fizz'>"+word+"<div>"
+        console.log(i + word);
+       
 
     }
+    /**SE numero diviso 5 da resto 0 allora stampo Buzz */
     else if(i % 5 == 0){
-        console.log(i + ' Buzz');
+        let word = 'Buzz'
+        container.innerHTML += "<div class='square square-buzz'>"+word+"<div>"
+        console.log(i + word);
+        
     }
     else {
+    
+        container.innerHTML += "<div class='square square-common'>"+i+"<div>"
         console.log(i);
     }
+
+
+
+    
 }
+
